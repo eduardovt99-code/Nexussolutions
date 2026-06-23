01231964a7e76a83b21a3fc8c5bd72cf83115517
Eduardo <eduardovt99@gmail.com>
2026-06-23 17:23:54 +0200

    chore: bump version to 66 for cache bust

--- lib/demo_version.dart
+++ lib/demo_version.dart
@@ -1,5 +1,5 @@
 /// Incrementar en cada release publicada para forzar recarga de demo en web.
 class DemoVersion {
-  static const String build = '65';
-  static const String label = 'Demo v65';
+  static const String build = '66';
+  static const String label = 'Demo v66';
 }
